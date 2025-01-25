83bd35944a92b02d7741bd7fdbb5046e8a9136bd
malobs <malobamercy958@gmail.com>
2025-01-25 17:22:58 +0300

    building the cart page

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -49,6 +49,8 @@ export const {
 export default cartSlice.reducer;
 
 //redux selector
+
+export const getCart = (state) => state.cart.cart;
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
